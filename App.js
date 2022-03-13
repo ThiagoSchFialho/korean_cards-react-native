c14a87209cbc71376b5f408afebba820d7d96007
ThiagoSchFialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2022-03-13 10:03:46 -0300

    starting the project

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, ScrollView, StatusBar, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Asap_400Regular, Asap_700Bold} from '@expo-google-fonts/asap';
+import { textShadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Asap_400Regular,
+    Asap_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.learnselection}>
+      <View style={styles.learnmenu}>
+        <View style={styles.theme}><Text style={styles.text}>casa</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>escola</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>praia</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>trabalho</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>animai</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>super mercado</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>cozinha</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>cores</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>escritorio</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>escola</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>parque</Text></View>
+        <View style={styles.theme}><Text style={styles.text}>cinema</Text></View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  learnselection: {
+    paddingVertical: 30,
+    backgroundColor: '#F2923F',
   },
+  learnmenu: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 30,
+    marginBottom: 40,
+    marginHorizontal: 50,
+    justifyContent: 'space-between'
+  },
+  theme: {
+    backgroundColor: '#5334b7',
+    backgroundColor: '#fff',
+    marginBottom: 50,
+    borderRadius: 20,
+    width: '40%',
+    height: 110,
+  },
+  text: {
+    fontSize: 17,
+    fontFamily: 'arial',
+    lineHeight: 110,
+    textAlign: 'center',
+    color: 'black'
+  }
 });
