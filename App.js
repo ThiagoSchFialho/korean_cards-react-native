14fba5e63d6c539b201d9ceac5fe32f41941cce3
ThiagoSchFialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2022-03-20 15:20:26 -0300

    update 3
    Implementação de botões interativos

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, ScrollView, Text, View, TouchableWithoutFeedback, Alert } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, TouchableWithoutFeedback, Pressable } from 'react-native';
 import { useFonts, Asap_400Regular, Asap_700Bold} from '@expo-google-fonts/asap';
 import AppLoading from 'expo-app-loading';
 import { textShadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const App = () => {
+  //O trecho de codigo visto abaixo é usado para carregar corretamente as fontes personalizadas
   let [fontsLoaded] = useFonts({
     Asap_400Regular,
     Asap_700Bold,
@@ -13,87 +14,192 @@ const App = () => {
     return <AppLoading/>;
   }
 
-
   return (
     <View>
       
       <ScrollView style={styles.learnSelection}>
         <View style={styles.learnMenu}>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              casa
-            </Text>
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('casa')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            casa
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              comida
-            </Text>
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('comida')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            comida
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              trabalho
-            </Text>
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('trabalho')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            trabalho
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              compras
-            </Text>
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('compras')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            compras
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              educação
-            </Text>
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('educação')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            educação
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              transporte
-            </Text>
-          </View>
+        <View  style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('transporte')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            transporte
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              pessoas
-            </Text>
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('pessoas')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            pessoas
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              aparência
-            </Text>
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('aparência')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            aparência
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            <Text style={styles.text}>
-              lazer
-            </Text>
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('lazer')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+          <Text style={styles.text}>
+            lazer
+          </Text>
+                </Pressable>
+        </View>
 
-          <View style={styles.theme}>
-            {/* <TouchableWithoutFeedback onPress={() => Alert.alert('alk')}>
-            </TouchableWithoutFeedback>; */}
-          </View>
+        <View style={styles.themeContainer}>
+          <Pressable
+            onPress={() => alert('básico')}
+            style={({ pressed }) => [
+              {
+                marginTop: pressed ? 5 : 0,
+                marginBottom: pressed ? 45 : 50
+              },
+              styles.theme
+          ]}>
+            <Text style={styles.text}>
+              básico
+            </Text>
+          </Pressable>
+        </View>
 
         </View>
       </ScrollView>
 
       <View style={styles.bottomMenu}>
-        <View style={styles.viewMenu1}>
-          <Text style={styles.textMenu}>
-            aprender
-          </Text>
-        </View>
 
-        <View style={styles.viewMenu2}>
-          <Text style={styles.textMenu}>
-            praticar
-          </Text>
-        </View>
+        <TouchableWithoutFeedback onPress={() => alert('aprender')}>
+          <View style={styles.viewMenu1}>
+            <Text style={styles.textMenu}>
+              aprender
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        <TouchableWithoutFeedback onPress={() => alert('praticar')}>
+          <View style={styles.viewMenu2}>
+            <Text style={styles.textMenu}>
+              praticar
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
 
     </View>
@@ -112,19 +218,20 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginBottom: 40,
     marginHorizontal: 50,
-    backgroundColor: 'red',
     justifyContent: 'space-between'
   },
-  theme: {
-    backgroundColor: '#fff',
-    borderBottomColor: '#555',
-    borderBottomWidth: 3,
-    borderRightColor: '#555',
-    borderRightWidth: 3,
+  themeContainer: {
+    borderBottomColor: '#444',
+    borderBottomWidth: 50,
     borderRadius: 20,
     marginBottom: 50,
     width: '40%',
     height: 110,
+  },
+  theme: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    height: 105,
   },
   text: {
     fontSize: 17,
