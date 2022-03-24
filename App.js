4b17a35d044041dd23b6eccaf32a94d1bc7252d7
ThiagoSchFialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2022-03-24 20:20:50 -0300

    update 5
    Adição de duas novas  telas 'learn.js' e 'practice.js' e a implantação de navegação entre telas com o stack navigator

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
 import React, { useState } from 'react';
 import { StyleSheet, ScrollView, Text, View, TouchableWithoutFeedback, Pressable } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { useFonts, Asap_400Regular, Asap_700Bold} from '@expo-google-fonts/asap';
 import AppLoading from 'expo-app-loading';
-import { textShadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
-const Theme = ({title}) => (
-    <View style={styles.themeContainer}>
-      <Pressable
-        onPress={() => alert(<Text>{title}</Text>)}
-        style={({ pressed }) => [
-          {
-            marginTop: pressed ? 5 : 0,
-            marginBottom: pressed ? 45 : 50
-          },
-          styles.theme
-          ]}>
-          <Text style={styles.text}>
-            {title}
-          </Text>
-      </Pressable>
-    </View>
-);
+import LearnScreen from './screens/learn';
+import PracticeScreen from './screens/practice';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   //O trecho de codigo visto abaixo é usado para carregar corretamente as fontes personalizadas
@@ -33,110 +23,13 @@ const App = () => {
   }
 
   return (
-    <View>
-      
-      <ScrollView style={styles.learnSelection}>
-        <View style={styles.learnMenu}>
-
-          <Theme title='casa'/>
-          <Theme title='comida'/>
-          <Theme title='trabalho'/>
-          <Theme title='compras'/>
-          <Theme title='educação'/>
-          <Theme title='transporte'/>
-          <Theme title='pessoas'/>
-          <Theme title='aparência'/>
-          <Theme title='lazer'/>
-          <Theme title='básico'/>
-
-        </View>
-      </ScrollView>
-
-      <View style={styles.bottomMenu}>
-
-        <TouchableWithoutFeedback onPress={() => alert('aprender')}>
-          <View style={styles.viewMenu1}>
-            <Text style={styles.textMenu}>
-              aprender
-            </Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <TouchableWithoutFeedback onPress={() => alert('praticar')}>
-          <View style={styles.viewMenu2}>
-            <Text style={styles.textMenu}>
-              praticar
-            </Text>
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Learn' component={LearnScreen}/>
+        <Stack.Screen name='Practice' component={PracticeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 };
-
-const styles = StyleSheet.create({
-  learnSelection: {
-    paddingVertical: 30,
-    marginBottom: 55,
-    backgroundColor: '#eea533'
-  },
-  learnMenu: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 30,
-    marginBottom: 40,
-    marginHorizontal: 50,
-    justifyContent: 'space-between'
-  },
-  themeContainer: {
-    borderBottomColor: '#bfbfbf',
-    borderBottomWidth: 50,
-    borderRadius: 20,
-    marginBottom: 50,
-    width: '40%',
-    height: 110,
-  },
-  theme: {
-    backgroundColor: 'white',
-    borderRadius: 20,
-    height: 105,
-  },
-  text: {
-    fontSize: 17,
-    fontFamily: 'Asap_700Bold',
-    lineHeight: 110,
-    textAlign: 'center',
-    textTransform: 'capitalize',
-    color: 'black'
-  },
-  bottomMenu: {
-    flexDirection: 'row',
-    marginTop: -60,
-    height: 60,
-    borderTopColor: '#444',
-    borderTopWidth: 2,
-    backgroundColor: 'white',
-  },
-  viewMenu1: {
-    borderRightColor: '#444',
-    borderRightWidth: 1,
-    width: '50%'
-  },
-  viewMenu2: {
-    borderLeftColor: '#444',
-    borderLeftWidth: 1,
-    width: '50%'
-  },
-  textMenu: {
-    fontSize: 17,
-    fontFamily: 'Asap_700Bold',
-    lineHeight: 60,
-    textAlign: 'center',
-    textTransform: 'capitalize',
-    color: 'black'
-  }
-});
-
 
 export default App;
---
+++ screens/practice.js
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, Text, View, TouchableWithoutFeedback, Pressable } from 'react-native';
+
+const Theme = ({title}) => (
+  <View style={styles.themeContainer}>
+    <Pressable
+      onPress={() => alert(<Text>{title}</Text>)}
+      style={({ pressed }) => [
+        {
+          marginTop: pressed ? 5 : 0,
+          marginBottom: pressed ? 45 : 50
+        },
+        styles.theme
+        ]}>
+        <Text style={styles.text}>
+          {title}
+        </Text>
+    </Pressable>
+  </View>
+);
+ 
+function PracticeScreen({navigation}) {
+  return(
+    <View>
+      <ScrollView style={styles.themeSelection}>
+        <View style={styles.themeMenu}>
+
+          <Theme title='casa'/>
+          <Theme title='comida'/>
+          <Theme title='trabalho'/>
+          <Theme title='compras'/>
+          <Theme title='educação'/>
+          <Theme title='transporte'/>
+          <Theme title='pessoas'/>
+          <Theme title='aparência'/>
+          <Theme title='lazer'/>
+          <Theme title='básico'/>
+
+        </View>
+      </ScrollView>
+
+      <View style={styles.bottomMenu}>
+        <TouchableWithoutFeedback onPress={() => navigation.navigate('Learn')}>
+          <View style={styles.viewMenu1}>
+            <Text style={styles.textMenu}>
+              aprender
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        <TouchableWithoutFeedback onPress={() => navigation.navigate('Practice')}>
+          <View style={styles.viewMenu2}>
+            <Text style={styles.textMenuSelected}>
+              praticar
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  themeSelection: {
+    paddingVertical: 30,
+    marginBottom: 55,
+    backgroundColor: '#5334b7'
+  },
+  themeMenu: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 30,
+    marginBottom: 40,
+    marginHorizontal: 50,
+    justifyContent: 'space-between'
+  },
+  themeContainer: {
+    borderBottomColor: '#bfbfbf',
+    borderBottomWidth: 50,
+    borderRadius: 20,
+    marginBottom: 50,
+    width: '40%',
+    height: 110,
+  },
+  theme: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    height: 105,
+  },
+  text: {
+    fontSize: 17,
+    fontFamily: 'Asap_700Bold',
+    lineHeight: 110,
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    color: 'black'
+  },
+  bottomMenu: {
+    flexDirection: 'row',
+    marginTop: -60,
+    height: 60,
+    borderTopColor: '#444',
+    borderTopWidth: 2,
+    backgroundColor: 'white',
+  },
+  viewMenu1: {
+    borderRightColor: '#444',
+    borderRightWidth: 1,
+    width: '50%'
+  },
+  viewMenu2: {
+    borderLeftColor: '#444',
+    borderLeftWidth: 1,
+    width: '50%'
+  },
+  textMenu: {
+    fontSize: 17,
+    fontFamily: 'Asap_700Bold',
+    lineHeight: 60,
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    color: 'black'
+  },
+  textMenuSelected: {
+    fontSize: 23,
+    fontFamily: 'Asap_700Bold',
+    lineHeight: 60,
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    textDecorationLine: 'underline',
+    color: '#5334b7'
+  }
+});
+
+export default PracticeScreen;
